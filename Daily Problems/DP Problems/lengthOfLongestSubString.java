c6d3bf6dd7631746d45bed78a64927af954e2efd
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-06-05 11:01:55 +0530

    add commented-out implementation for printing the longest common substring

--- Daily Problems/DP Problems/lengthOfLongestSubString.java
+++ Daily Problems/DP Problems/lengthOfLongestSubString.java
@@ -24,3 +24,28 @@ public class lengthOfLongestSubString {
         return ans;
     }
 }
+//for printing the longest common substring
+/*
+        int n=s1.length();
+        int m=s2.length();
+        int[][] dp = new int[n+1][m+1];
+        int maxLength=0;
+        int endIndex=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                 if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i[j]]=1+dp[i-1][j-1];
+                    if(dp[i][j]>maxLength){
+                        maxLength=dp[i][j];
+                        endIndex=i;
+                    }
+                }
+                    else{
+                        dp[i][j]=0;
+                    }
+            }
+        }
+        return s1.substring(endIndex-maxLength,endIndex);
+
+
+*/
