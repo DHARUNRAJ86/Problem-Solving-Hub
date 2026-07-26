12a0f0fed04ac75217ea3d19879187135f29d573
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-07-26 20:37:04 +0530

    add implementation for MaximumProduct class to calculate the maximum product of three numbers in an array

---
+++ Daily Problems/MyPractice/MaximumProduct.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class MaximumProduct {
+    public static void main(String[] args){
+        int[] num={1,2,3,4};
+        System.out.println(maximumProduct(num));
+    }
+    public static int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return Math.max(nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[n-1]);
+    }
+}
