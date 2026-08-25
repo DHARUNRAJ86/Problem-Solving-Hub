8ec674e1b36e810f80dec04fda37498a046acc02
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-08-25 17:15:47 +0530

    add implementation for MissingMultiple class to find the smallest missing multiple of k in an array

---
+++ Daily Problems/Practice/MissingMultiple.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+
+class MissingMultiple{
+    public static void main(String[] args) {
+        int[] nums={8,2,3,4,6};
+        int k=2;
+        System.out.println(missingMultiple(nums,k));
+    }
+    public static int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        boolean isTrue=true;
+        int missing=k;
+        while(isTrue){
+            if(!set.contains(missing)){
+                  return missing;
+            }
+            else{
+                missing+=k;
+            }
+        }
+        return -1;
+    }
+}
