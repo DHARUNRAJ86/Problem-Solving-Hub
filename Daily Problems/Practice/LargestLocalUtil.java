8962d086bbf11a66d15eb6ee58341b9a58a588c9
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-08-28 12:30:31 +0530

    add implementation for LargestLocalUtil class to find the largest local values in a 2D array

---
+++ Daily Problems/Practice/LargestLocalUtil.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class LargestLocalUtil{
+    public static void main(String[] args) {
+        int[][] arr={{9,9,8,1},{5,6,2,6},{8,2,6,4},{6,2,2,2}};
+        int[][] ans=largestLocal(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static int[][] largestLocal(int[][] grid) {
+        int n = grid.length; 
+        int m = n-2;
+        int[][] ans = new int[m][m];
+        for (int i = 0 ; i < m ; i++) {
+            for (int j = 0 ; j < m ; j++) {
+                ans[i][j] = largestLocalUtil(grid, i, j);
+            }
+        }
+        return ans;
+    }
+    public static int largestLocalUtil(int[][] grid, int x, int y) {
+        int max = 0; 
+        for (int i = x ; i < x+3 ; i++) {
+            for (int j = y ; j < y+3 ; j++) {
+                max = Math.max(max, grid[i][j]);
+            }
+        }
+        return max;
+    }
+}
