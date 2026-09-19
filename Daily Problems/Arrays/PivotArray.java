ec96cce8dace343ca21118112c625c03c90406db
Dharunraj P <pdharunraj@gmail.com>
2026-09-19 14:15:59 +0530

    Add pivotArray method to rearrange array elements
    Implement pivotArray method to rearrange elements based on a pivot value.

---
+++ Daily Problems/Arrays/PivotArray.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int count=0;
+        for(int num:nums){
+            if(num<pivot){
+                list.add(num);
+            }
+            if(num==pivot){
+                count++;
+            }
+        }
+        for(int i=0;i<count;i++){
+            list.add(pivot);
+        }
+        for(int num:nums){
+            if(num>pivot){
+                list.add(num);
+            }
+        }
+        int[] res = new int[list.size()];
+        int ind=0;
+        for(int num:list){
+            res[ind++]=num;
+        }
+        return res;
+    }
+}
